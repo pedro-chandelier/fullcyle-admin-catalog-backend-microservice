d662fd6ae551c4855015c14790ae201d58d6bf56
pedro_haeser <haeser.pedro@gmail.com>
2022-03-20 02:14:53 -0300

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	singleQuote: true,
+	arrowParens: 'avoid',
+	semi: false,
+	trailingComma: 'none',
+	useTabs: false,
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	jsxBracketSameLine: false,
+	printWidth: 130,
+	proseWrap: 'preserve'
+}
