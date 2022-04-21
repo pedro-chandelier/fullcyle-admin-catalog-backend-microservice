1d7e661caa46e975a9e461c9cefa5a30130c3d1c
Pedro <haeser.pedro@gmail.com>
2022-04-20 23:21:09 -0300

    config test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,20 @@
 export default {
   clearMocks: true,
-  coverageProvider: 'v8',
   rootDir: 'src',
   testRegex: ['.*\\..*spec\\.ts$', '.*\\..*test\\.ts$'],
   transform: {
     '^.+\\.ts?$': ['@swc/jest']
   },
   setupFilesAfterEnv: ['./@seedwork/domain/tests/validations.extend.ts'],
-  coverageDirectory: '__coverage__'
+  coverageProvider: 'v8',
+  coverageDirectory: '<rootDir>/../__coverage__',
+  coverageReporters: ['json', 'html'],
+  coverageThreshold: {
+    global: {
+      statements: 95,
+      branches: 95,
+      functions: 95,
+      lines: 95
+    }
+  }
 }
